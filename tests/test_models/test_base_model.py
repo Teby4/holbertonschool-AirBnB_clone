85398c25726c0230a0e44f88e4d824832880bb2e
root <felipeoliveramantero@gmail.com>
2024-02-26 12:44:51 -0800

    test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+from models.base_model import BaseModel
+import unittest
+from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_init(self):
+        model = BaseModel()
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_str(self):
+        model = BaseModel()
+        out = "[{}] ({}) <{}>".format(type(model).__name__, model.id, model.__dict__)
+        self.assertEqual(str(model), out)
+
+    def test_to_dict(self):
+        model = BaseModel()
+        dict = model.to_dict()
+        self.assertIsInstance(dict["created_at"], str)
+        self.assertIsInstance(dict["updated_at"], str)
+
+    def test_save(self):
+        m = BaseModel()
+        date = m.updated_at
+        m.save()
+        self.assertNotEqual(date, m.updated_at)
+
+if __name__ == '__main__':
+    unittest.main()
