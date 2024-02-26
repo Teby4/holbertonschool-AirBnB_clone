41fa93c7a80071a9a9b31bfe82fcb81676d3f860
root <felipeoliveramantero@gmail.com>
2024-02-26 12:00:10 -0800

    task 0

--- models/base_model.py
+++ models/base_model.py
@@ -13,8 +13,8 @@ class BaseModel():
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
         return "[{}] ({}) <{}>".format(type(self).__name__, self.id, self.__dict__)
@@ -25,4 +25,4 @@ class BaseModel():
         return r
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now().isoformat()
