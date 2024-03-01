c044d0325cd3bf3c659ff357194b1110b56ad1d3
root <felipeoliveramantero@gmail.com>
2024-03-01 07:06:29 -0800

    console .py first task

--- console.py
+++ console.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+Write a program called console.py that contains the entry point of the command interpreter:
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    
+    """
+    prompt = '(hbnb) '
+
+    def emptyline(self):
+        pass
+
+    def do_quit(self, arg):
+        exit()
+    
+    def do_EOF(self, arg):
+        exit()
+        
+    def do_help(self, arg):
+        help_messages = {
+            "quit" : "Quit command to exit the program",
+            "EOF" : "exit the program",
+            "help" : "displays help message",
+            "" : "\nDocumented commands (type help <topic>)\n\
+========================================\n\
+EOF help quit\n\n"
+        }
+        print(help_messages[arg])
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
