e656fb914c72d6143c26fa94a55da6200e3b8201
root <felipeoliveramantero@gmail.com>
2024-03-03 08:19:36 -0800

    test

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -19,11 +19,11 @@ class TestFileStorage(unittest.TestCase):
         except FileNotFoundError:
             pass
 
-    def test_all_empty(self):
+    def test_all(self):
         # Test the 'all' method when there are no objects
         self.assertEqual(self.file_storage.all(), {})
 
-    def test_new_and_all(self):
+    def test_new(self):
         # Test the 'new' and 'all' methods
         model1 = BaseModel()
         model2 = BaseModel()
@@ -38,7 +38,7 @@ class TestFileStorage(unittest.TestCase):
         self.assertIn("BaseModel." + model2.id, objects)
         self.assertIn("BaseModel." + model3.id, objects)
 
-    def test_save_and_reload(self):
+    def test_save(self):
         # Test the 'save' and 'reload' methods
         model1 = BaseModel()
         model2 = BaseModel()
@@ -58,6 +58,23 @@ class TestFileStorage(unittest.TestCase):
         new_file_storage.reload()
 
         # Check if the reloaded objects match the original ones
+        self.assertEqual(self.file_storage.all(), new_file_storage.all())
+
+    def test_reload(self):
+        model1 = BaseModel()
+        model2 = BaseModel()
+        model3 = BaseModel()
+
+        self.file_storage.new(model1)
+        self.file_storage.new(model2)
+        self.file_storage.new(model3)
+
+        self.file_storage.save()
+        
+        new_file_storage = FileStorage()
+
+        new_file_storage.reload()
+        
         self.assertEqual(self.file_storage.all(), new_file_storage.all())
 
 if __name__ == '__main__':
